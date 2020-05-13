e5824149791045f4e2bc9f914e44f755cc2f8eb4
Dzmitry Karashkou <Dzmitry_Karashkou@epam.com>
2020-05-13 17:02:37 +0300

    initial commit

---
+++ src/test/java/HelloWebDriver.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWebDriver {
+    public static void main (String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://seleniumhq.org");
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
